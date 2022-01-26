6677dd11e6c39f7f4db767bd29e32c074fd66786
shanotinohito <fuzakebito@gmail.com>
2022-01-26 19:09:57 +0900

    lualine

--- lua/lualine/themes/antibalus.lua
+++ lua/lualine/themes/antibalus.lua
@@ -1,7 +1,6 @@
 local colors = {
   bg = '#27262c',
-  black0 = '#34333e',
-  black1 = '#42404e',
+  black = '#34333e',
   red = '#cc5655',
   green = '#4aad78',
   yellow = '#c99a68',
@@ -13,38 +12,38 @@ local colors = {
 
 return {
   normal = {
-    a = {bg = colors.magenta, fg = colors.black0, gui = 'bold'},
-    b = {bg = colors.white, fg = colors.black0},
-    c = {bg = colors.black1, fg = colors.white}
+    a = {bg = colors.magenta, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.white, fg = colors.black},
+    c = {bg = colors.black, fg = colors.white}
   },
   insert = {
-    a = {bg = colors.cyan, fg = colors.black0, gui = 'bold'},
-    b = {bg = colors.white, fg = colors.black0},
-    c = {bg = colors.black1, fg = colors.white}
+    a = {bg = colors.cyan, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.white, fg = colors.black},
+    c = {bg = colors.black, fg = colors.white}
   },
   visual = {
-    a = {bg = colors.red, fg = colors.black0, gui = 'bold'},
-    b = {bg = colors.white, fg = colors.black0},
-    c = {bg = colors.black1, fg = colors.white}
+    a = {bg = colors.red, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.white, fg = colors.black},
+    c = {bg = colors.black, fg = colors.white}
   },
   replace = {
-    a = {bg = colors.yellow, fg = colors.black0, gui = 'bold'},
-    b = {bg = colors.white, fg = colors.black0},
-    c = {bg = colors.black1, fg = colors.white}
+    a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.white, fg = colors.black},
+    c = {bg = colors.black, fg = colors.white}
   },
   command = {
-    a = {bg = colors.green, fg = colors.black0, gui = 'bold'},
-    b = {bg = colors.white, fg = colors.black0},
-    c = {bg = colors.black1, fg = colors.white}
+    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.white, fg = colors.black},
+    c = {bg = colors.black, fg = colors.white}
   },
   terminal = {
-    a = {bg = colors.cyan, fg = colors.black0, gui = 'bold'},
-    b = {bg = colors.white, fg = colors.black0},
-    c = {bg = colors.black1, fg = colors.white}
+    a = {bg = colors.cyan, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.white, fg = colors.black},
+    c = {bg = colors.black, fg = colors.white}
   },
   inactive = {
-    a = {bg = colors.black1, fg = colors.grey},
-    b = {bg = colors.black1, fg = colors.grey},
-    c = {bg = colors.black1, fg = colors.grey}
+    a = {bg = colors.black, fg = colors.grey},
+    b = {bg = colors.black, fg = colors.grey},
+    c = {bg = colors.black, fg = colors.grey}
   }
 }
